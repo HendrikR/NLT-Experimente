a2a5a6442918fb6a4ed8931e0de9a0fa1844b72a
Hendrik Radke <hermes9@web.de>
2013-09-27 15:06:47 +0200

    Itemliste verbessert - Falsche Zuordnung der Item-Daten (icon) behoben - Falsche Zuordnung: "itemid" ist in Wirklichkeit "icon" - Falsche Zuordnung: "icon" ist in Wirklichkeit "fk_idx", ein Index in eine andere Tabelle - HTML-Ausgabe der Daten als Tabelle - Plausible Erklärung für unk3: Genus

--- itemview.rb
+++ itemview.rb
@@ -4,7 +4,7 @@
 
 # Insgesamt gibt es 352 Items, davon 84 Waffen und 36 Rüstungsteile.
 class Item
-  attr_accessor :name, :itemcode, :typ, :typ2, :subtyp, :gewicht, :icon, :preis, :magic, :i_entry, :haeufigkeit, :unk3
+  attr_accessor :index, :name, :icon, :typ, :typ2, :subtyp, :gewicht, :fk_index, :preis, :magic, :i_entry, :haeufigkeit, :genus
   def name_s
     arr = @name.split('.')
     if arr.size == 1 then arr[0]
@@ -73,16 +73,24 @@ class Item
       "unbekannt:"+subtyp.to_s
     end
   end
+  def genus_s
+    case(genus)
+      when 0; "Der"
+      when 1; "Die"
+      when 2; "Das"
+      else    "Error"
+    end
+  end
 end
 
 f_data = File.open("/home/hendrik/repos/BrightEyes/tools/nltpack/out-roa2/ITEMS.DAT", "rb")
 f_name = File.open("/home/hendrik/repos/BrightEyes/tools/nltpack/out-roa2/ITEMS.LTX", "rb")
 f_entry= File.open("/home/hendrik/repos/BrightEyes/tools/nltpack/out-roa2/I_ENTRY.DAT", "rb")
-puts f_entry.size
 
 itemlist = []
-f_data.read(2)
+#f_data.read(2)
 #f_data.read(14) # Dummy-Item
+index = 0
 while(data = f_data.read(14))
   break if data.size < 14
   break if f_name.eof?
@@ -91,22 +99,50 @@ while(data = f_data.read(14))
   while((c = f_name.read(1)) != "\0") do name+=c; end
   i = Item.new
   i.name = name
-  i.i_entry  = f_entry.readbyte # Wenn i_entry==1, wird der Fund im Tagebuch vermerkt.
+  if not f_entry.eof?
+    i.i_entry  = f_entry.readbyte # Wenn i_entry==1, wird der Fund im Tagebuch vermerkt.
+  else
+    i.i_entry  = 0
+  end
   
-  i.typ         = data[0x00]
-  i.typ2        = data[0x01]
-  i.subtyp      = data[0x02]
-  i.icon        = data[0x03] # Bild-Index oder ähnlich?
-  i.gewicht     = data[0x04] | (data[0x05] << 8)
-  i.preis       = data[0x08] * (data[0x07] << 8 | data[0x06])
-  i.haeufigkeit = data[0x09] # Siehe [[http://www.crystals-dsa-foren.de/showthread.php?tid=700&pid=125835]]
-  i.magic       = data[0x0A]
-  i.unk3        = data[0x0B] # Flag/Enum, ist entweder 0,1 oder 2. 2 könnte "benutzbar" sein oder so, 1 ist seltsam.
-  i.itemcode    = data[0x0C] << 8 | data[0x0D]
+  i.index       = index
+  i.icon        = data[0x00] | data[0x01] << 8
+  i.typ         = data[0x02]
+  i.typ2        = data[0x03]
+  i.subtyp      = data[0x04]
+  i.fk_index    = data[0x05] # Index in eine andere Tabelle (Rüstungs- oder Waffenwerte, ...)
+  i.gewicht     = data[0x06] | (data[0x07] << 8)
+  i.preis       = data[0x0A] * (data[0x09] << 8 | data[0x08])
+  i.haeufigkeit = data[0x0B] # Siehe [[http://www.crystals-dsa-foren.de/showthread.php?tid=700&pid=125835]]
+  i.magic       = data[0x0C]
+  i.genus       = data[0x0D] # Grammatikalisches Geschlecht (im Deutschen): 0=m, 1=f, 2=n
 
   itemlist << i
+  index+= 1
 end
+itemlist.pop # Das letzte Item, "Dukaten", hat keinen sinnvollen Eintrag.
 
-itemlist.each do |item|
-  printf("%04x, %s, %s, %02x\n", item.itemcode, item.name_s + (item.magic==1 ? "*" : ""), item.typ_s, item.unk3)
+puts "<!DOCTYPE HTML>"
+puts "<html><head>"
+puts "<title>Liste der Gegenstände für DSA2</title>"
+puts "<meta http-equiv='content-type' content='text/html; charset=utf-8' />"
+puts "</head><body><table border='1'>"
+puts "<tr><th>Index</th><th>Name</th><th>Item-Typ</th><th>Gewicht (U)</th><th>Häufigkeit</th><th>Flags</th><th>Fk_Index</th></tr>"
+def d(val)
+  puts "<td>#{val.to_s}</td>"
 end
+itemlist.each do |item|
+  #printf("%04x, %s, %s, %02x\n", item.icon, item.name_s + (item.magic==1 ? "*" : ""), item.typ_s, item.unk3)
+  puts "<tr>"
+  d sprintf("%x", item.index)
+  d sprintf("<img src='item-S00I%03d.png' width='32'>", item.icon) + " " +
+    item.genus_s + " " + 
+    item.name_s
+  d item.typ_s+":" + item.typ2_s + "("+item.subtyp_s+")"
+  d item.gewicht
+  d item.haeufigkeit
+  d (item.magic==1 ? "M" : "&nbsp;") + (item.i_entry==1 ? "T" : "&nbsp;")
+  d sprintf("%x", item.fk_index)
+  puts "</tr>"
+end
+puts "</table></body></html>"
